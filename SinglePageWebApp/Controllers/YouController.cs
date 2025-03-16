ecfd337bcbe955cb31d2f2c2091913d53c4c9e28
tornikeshavishvili <shavishvilitornike@gmail.com>
2025-03-16 11:15:04 +0400

    Add YouCOntroller with basic authentication. It recieves json via post and returns Json Object

---
+++ SinglePageWebApp/Controllers/YouController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Newtonsoft.Json.Linq;
+using Owin;
+using SinglePageWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+
+namespace SinglePageWebApp.Controllers {
+
+    [BasicAuthentication]
+    //[System.Web.Http.Authorize]
+    public class YouController : ApiController
+    {
+      
+
+        public YouController()
+        {
+        }
+
+        // GET api/You
+        [System.Web.Http.HttpPost]
+        public JsonResult<GetViewModel> MyFunction([FromBody] JObject myObject)
+        {
+            
+            return Json(new GetViewModel() { Hometown = ((string)myObject.GetValue("myValue")) });
+             
+        }
+    }
+}
